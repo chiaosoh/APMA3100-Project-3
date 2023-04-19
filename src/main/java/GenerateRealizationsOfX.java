b7afe3f5f71f510f9452398864f94f984236004f
Chiao Soh <chiao.soh@gmail.com>
2023-04-19 00:58:01 -0400

    Copied over old linear congruential random number generator with new variables

---
+++ src/main/java/GenerateRealizationsOfX.java
@@ -0,0 +1,19 @@
+public class GenerateRealizationsOfX {
+    static final int STARTING_SEED = 1000;
+    static final int MULTIPLIER = 24693;
+    static final int INCREMENT = 3967;
+    static final int MODULUS = 262144;
+    public static double generateRandomProbability(int index) {
+        long randomNumber = STARTING_SEED;
+        for(int i = 0; i < index; i++) {
+            randomNumber = getRandomNumber(randomNumber);
+        }
+        return getRandomProbability(randomNumber);
+    }
+    public static int getRandomNumber(long previousNumber) {
+        return (int)((previousNumber * MULTIPLIER + INCREMENT) % MODULUS);
+    }
+    public static double getRandomProbability(long randomNumber) {
+        return (double) (randomNumber)/MODULUS;
+    }
+}
